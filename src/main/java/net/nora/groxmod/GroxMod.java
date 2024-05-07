d8316e15f7eebb992d18567e8d355224f867f1be
HyenaAbuse <al.ptakh@gmail.com>
2024-05-07 13:47:19 +0300

    Added 2 items

--- src/main/java/net/nora/groxmod/GroxMod.java
+++ src/main/java/net/nora/groxmod/GroxMod.java
@@ -1,18 +1,19 @@
 package net.nora.groxmod;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.nora.groxmod.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -26,6 +27,8 @@ public class GroxMod {
 
     public GroxMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
@@ -42,6 +45,10 @@ public class GroxMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.CUP);
+            event.accept(ModItems.PILOT);
+        }
 
     }
 
---
+++ src/main/java/net/nora/groxmod/item/ModItems.java
@@ -0,0 +1,24 @@
+package net.nora.groxmod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.nora.groxmod.GroxMod;
+
+public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, GroxMod.MOD_ID);
+    public static final RegistryObject<Item> CUP = ITEMS.register("cup",
+            ()-> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> PILOT = ITEMS.register("pilot",
+            ()-> new Item(new Item.Properties()));
+
+
+    public static void  register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
